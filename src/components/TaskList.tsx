8a77460d1ad1482a010866be710e7b3acc0e6107
Talles <talles17.a@hotmail.com>
2021-03-18 14:06:00 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,24 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle != '') {
+      const task = { id: Math.random() * 1000, title: newTaskTitle, isComplete: false };
+      const updateTasks = [...tasks, task];
+      setTasks(updateTasks);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTasks = tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task);
+    setTasks(updateTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const updateTasks = tasks.filter(task => task.id !== id);
+    setTasks(updateTasks);
   }
 
   return (
